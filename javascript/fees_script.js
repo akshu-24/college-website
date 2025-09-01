2d67c578bd4b9a8c3d5804ec6ff9a2c67b39a45b
akshu-24 <akshayamanikandan24@gmail.com>
2025-09-01 21:53:53 +0530

    Add files via upload

---
+++ javascript/fees_script.js
@@ -0,0 +1,109 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+    if(inputVal==1){
+        document.getElementById("demo").innerHTML ="Agalya M";
+        document.getElementById("demo1").innerHTML ="10000";
+    }
+
+    else if(inputVal==2){
+        document.getElementById("demo").innerHTML ="Akalya R";
+        document.getElementById("demo1").innerHTML ="10000";
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="Akshaya M";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="Archana V";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==5)
+    {
+        document.getElementById("demo").innerHTML ="Arivumathi A";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==6)
+    {
+        document.getElementById("demo").innerHTML ="Atchaya R";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="Dharma D";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="Dharshini K";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==9)
+    {
+        document.getElementById("demo").innerHTML ="Janani K";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="Kavi M";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="Kaviya C";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==12)
+    {
+        document.getElementById("demo").innerHTML ="Mahalakshimi R";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="Monika G";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="Priya D";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="Priyanka S";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="Sumitha S";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="Swetha D";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="Shalini R";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="Yaalini T";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="Yashika M";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    
+}
+
